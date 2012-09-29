da18669a01929962f983978b24b403641d65e26d
bijendra <bijendra@sourcebits.com>
2012-09-30 03:19:37 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapEx1::Application.config.secret_token = '79e22b026ab357be85bffa618ae8327bbbabbb45f5ddac8ecbfdeb8a57a7c6ec29e2a7b8ebf19fab10111c07d573b517f832c2108f7e41e61a82293c07c09113'
